ac918637a2bbb95521fd53bc499fdf74268f35ed
nadezhda.lemekhova <nal.spb@gmail.com>
2020-05-11 16:18:24 +0300

    4.5 - Compare the size of ArrayList

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupCreationTests extends TestBase {
 
   @Test
@@ -11,12 +13,13 @@ public class GroupCreationTests extends TestBase {
 
     app.getNavigationHelper().gotoGroupPage();
 
-    int before = app.getGroupHelper().getGroupCount();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+
     app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
 
-    int after = app.getGroupHelper().getGroupCount();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
 
-    Assert.assertEquals(after, before+1);
+    Assert.assertEquals(after.size(), before.size()+1);
   }
 
 }
