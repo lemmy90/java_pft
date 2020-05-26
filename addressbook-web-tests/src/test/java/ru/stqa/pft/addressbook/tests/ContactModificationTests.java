8da61a3eb9cb3cdff8d4a35865dd91aa55084b96
nadezhda.lemekhova <nal.spb@gmail.com>
2020-05-26 10:25:34 +0300

    последние правки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -31,9 +31,11 @@ public class ContactModificationTests extends TestBase {
 
     before.remove(before.size()-1);
     before.add(contact);
+
     Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
     before.sort(byId);
     after.sort(byId);
+
     Assert.assertEquals(before, after);
 
 
