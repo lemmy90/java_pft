7be67650bb2666b733f8ae54d30fdd83618a450c
nadezhda.lemekhova <nal.spb@gmail.com>
2020-05-27 18:38:36 +0300

    5.5 Using unic id for Groups

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,11 +1,10 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
@@ -14,26 +13,18 @@ public class GroupCreationTests extends TestBase {
 
     app.goTo().groupPage();
 
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("test2");
     app.group().create(group);
 
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
 
     Assert.assertEquals(after.size(), before.size()+1);
 
-    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId()); //with lambda
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
 
-    //сортировка по id
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-
     Assert.assertEquals(before,after);
-
-
-    //Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after)); //преобразование списка во множество
   }
 
 }
