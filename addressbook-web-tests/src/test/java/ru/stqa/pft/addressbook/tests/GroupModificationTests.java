ed7f78255e68ca9fb143b762a08c4b1d68b758b4
nadezhda.lemekhova <nal.spb@gmail.com>
2020-06-08 22:50:28 +0300

    7.4 Comparison with DB data

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -19,7 +19,8 @@ public class GroupModificationTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions(){
     app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+
+    if (app.db().groups().size() == 0) {
       app.group().create(new GroupData().withName("test1"));
     }
   }
@@ -27,7 +28,7 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
 
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData modifiedGroup = before.iterator().next();
 
     GroupData group = new GroupData()
@@ -36,7 +37,7 @@ public class GroupModificationTests extends TestBase {
     app.group().modify(group);
     assertThat(app.group().count(),equalTo(before.size()));
 
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     before.remove(modifiedGroup);
     before.add(group);
 
