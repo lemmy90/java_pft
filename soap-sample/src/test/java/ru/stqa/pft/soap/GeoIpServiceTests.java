b21caec1127d5a2ce1eb9bd615ce23c10893896a
nadezhda.lemekhova <nal.spb@gmail.com>
2020-09-24 13:42:54 +0300

    9.2 GeoIpServiceTests (not fully done)

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+import com.lavasoft.GetIpLocation;
+import com.lavasoft.GetIpLocationResponse;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp(){
+
+//        GeoIp geoIp = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("91.221.102.34");
+//        assertEquals(geoIp.get);
+
+    }
+}
