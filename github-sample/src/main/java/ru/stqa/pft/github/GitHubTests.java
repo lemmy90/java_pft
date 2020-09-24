d11758b607aaf9f9b8bd53ef41c8784172c1a68d
nadezhda.lemekhova <nal.spb@gmail.com>
2020-09-24 22:33:02 +0300

    9.6 Not fully done
    Need to understand how to generate OAuth key

---
+++ github-sample/src/main/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.github;
+
+import com.jcabi.github.Github;
+import com.jcabi.github.RtGithub;
+import org.testng.annotations.Test;
+
+public class GitHubTests {
+
+    @Test
+    public void testCommits() {
+
+        Github github = new RtGithub(".. your OAuth token ..");
+
+    }
+
+
+}
